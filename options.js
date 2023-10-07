8d174757513381557ddae098e3035a3adb58f39e
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2023-10-07 18:12:38 +0900

    Update options.js

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 var speeds = [];
-var defaults = [0.50, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 5, 7.5, 10, 13, 16];
+var defaults = [0.50, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 5, 7.5, 10, 13, 16, 32, 64, 128];
 
 chrome.storage.sync.get("udemyChangerSpeeds", function (data) {
 	var loadedSpeeds = data['udemyChangerSpeeds'];
